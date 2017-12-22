7d07acf2eb897da9bdb457e3c8fa5311c9ec857a
Nic <Wanavit>
2017-12-22 23:44:07 +0900

    add codes for hatari

--- js/luckydraw.js
+++ js/luckydraw.js
@@ -1,7 +1,30 @@
 function drawNext() {
-  var lastdraw = $("#luckyperson").html();
-  var alllucky = $("#drawhistory").html();
-  $("#drawhistory").html(lastdraw + '<br />' + alllucky);
-  var personname = data[Math.floor(Math.random() * data.length)];
-  $("#luckyperson").html(personname);
+	return function(){
+	  var lastdraw = $("#luckyperson").html();
+	  var alllucky = $("#drawhistory").html();
+	  $("#drawhistory").html(lastdraw + '<br />' + alllucky);
+	  var personname = data[Math.floor(Math.random() * data.length)];
+	  $("#luckyperson").html(personname);
+  	}
+}
+function countdown() {
+	$("#luckyperson").html(1);
+	setTimeout(countnumber(1), 2000);
+	setTimeout(countnumber(2), 4000);
+	setTimeout(realdraw(), 6000);
+	setTimeout(drawNext(), 7000);
+}
+function countnumber(number){
+	return function(){
+		$("#luckyperson").html(number+1);
+	}
+}
+function startloop() {
+	countdown()
+}
+
+function realdraw(){
+	return function(){
+		$("#luckyperson").html(personname);
+	}
 }
